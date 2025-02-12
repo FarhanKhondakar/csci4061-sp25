5091103789ea81e158a6172c8d7bc8469a20aefb
Farhan Khondakar <farhan.khondakar@gmail.com>
2025-02-12 01:02:05 -0600

    large refactoring and cleanup

--- projects/proj1-code/minitar.c
+++ projects/proj1-code/minitar.c
@@ -134,9 +134,95 @@ void close_file(FILE *file, const char *msg) {
     }
 }
 
+// Convert Octal String To Size_t
+// This is used to calculate the size of the file, ensuring that we have the correct size.
+int convert_octal_to_size_t(const char *octal_string, size_t *size) {
+    char *end_pointer;
+
+    // We Use strtoull to convert the Octal String to an Unsigned Long Long.
+    // Furthermore, we use the end_pointer to check if the conversion was successful.
+    *size = strtoull(octal_string, &end_pointer, OCTAL_BASE);
+    if (*end_pointer != '\0') {
+        perror("Failed to convert octal string to size_t");
+        return -1;
+    }
+
+    return 0;
+}
+
+// Writes The Padding (of BLOCK_SIZE) To The Archive.
+int write_file_padding(FILE *tar_archive, size_t padding) {
+    char padding_buffer[BLOCK_SIZE] = {0};
+
+    // Write The Padding To The Archive.
+    // Else If The Padding Is Not Written, Return An Error.
+    if (fwrite(padding_buffer, 1, padding, tar_archive) != padding) {
+        perror("Failed to write to tar archive");
+        return -1;
+    }
+
+    return 0;
+}
+
+// Writes The Footer (Two Empty Blocks) To The Archive.
+int write_footer(FILE *tar_archive) {
+    // Add Two Empty Blocks (Footer) To The End Of The Archive.
+    char empty_footer[BLOCK_SIZE * NUM_TRAILING_BLOCKS] = {0};
+
+    // Write The Empty Footer To The Archive.
+    // If The Empty Footer Is Not Written To Exactly 1024 Bytes, Return An Error.
+    if (fwrite(empty_footer, 1, BLOCK_SIZE * NUM_TRAILING_BLOCKS, tar_archive) !=
+        BLOCK_SIZE * NUM_TRAILING_BLOCKS) {
+        perror("Failed to write to tar archive");
+        return -1;
+    }
+
+    return 0;
+}
+
+// Writes The File Contents To The Archive.
+int write_file_contents(FILE *tar_archive, FILE *input_file) {
+    char buffer[BLOCK_SIZE] = {0};
+    size_t bytes_fetched;
+
+    // Read The File Contents In Blocks Of 512 Bytes.
+    // If Bytes Fetched is 0, We Have Reached The End Of The File.
+    while ((bytes_fetched = fread(buffer, 1, BLOCK_SIZE, input_file)) > 0) {
+        // Write The File Contents To The Archive.
+        size_t bytes_written = fwrite(buffer, 1, bytes_fetched, tar_archive);
+
+        // If The Bytes Written Is Not Equal To The Bytes Fetched, Return An Error.
+        if (bytes_written != bytes_fetched) {
+            perror("Failed to write to tar archive");
+            return -1;
+        }
+
+        // If The File Contents is Less Than 512 Bytes, We need to add padding.
+        // This ensures that the file contents are written in blocks of 512 bytes (following the
+        // tar format).
+        if (bytes_fetched < BLOCK_SIZE) {
+            size_t padding = BLOCK_SIZE - bytes_fetched;
+
+            // Write The Padding To The Archive.
+            if (write_file_padding(tar_archive, padding) != 0) {
+                return -1;
+            }
+        }
+    }
+
+    // Check If The fread Function Failed To Read The File.
+    // If The Bytes Fetched Is 0 And ferror Returns An Error, Return An Error.
+    if (bytes_fetched == 0 && ferror(input_file) != 0) {
+        perror("Failed to read file");
+        return -1;
+    }
+
+    return 0;
+}
+
 int create_archive(const char *archive_name, const file_list_t *files) {
-    // Create A New Tar Archive with Write Permissions.
-    FILE *tar_archive = fopen(archive_name, "w");
+    // Create A New Tar Archive with Write Binary Permissions.
+    FILE *tar_archive = fopen(archive_name, "wb");
     if (tar_archive == NULL) {
         perror("Failed to create tar archive");
         return -1;
@@ -177,45 +263,7 @@ int create_archive(const char *archive_name, const file_list_t *files) {
         }
 
         // Write/Copy The File Contents To The Archive.
-        char buffer[BLOCK_SIZE] = {0};
-        char padding_buffer[BLOCK_SIZE] = {0};
-        size_t bytes_fetched;
-
-        // Read The File Contents In Blocks Of 512 Bytes.
-        // If Bytes Fetched is 0, We Have Reached The End Of The File.
-        while ((bytes_fetched = fread(buffer, 1, BLOCK_SIZE, input_file)) > 0) {
-            // Write The File Contents To The Archive.
-            size_t bytes_written = fwrite(buffer, 1, bytes_fetched, tar_archive);
-
-            // If The Bytes Written Is Not Equal To The Bytes Fetched, Return An Error.
-            if (bytes_written != bytes_fetched) {
-                perror("Failed to write to tar archive");
-                close_file(tar_archive, "Failed to close tar archive");
-                close_file(input_file, "Failed to close file");
-                return -1;
-            }
-
-            // If The File Contents is Less Than 512 Bytes, We need to add padding.
-            // This ensures that the file contents are written in blocks of 512 bytes (following the
-            // tar format).
-            if (bytes_fetched < BLOCK_SIZE) {
-                size_t padding = BLOCK_SIZE - bytes_fetched;
-
-                // Write The Padding To The Archive.
-                // Else If The Padding Is Not Written, Return An Error.
-                if (fwrite(padding_buffer, 1, padding, tar_archive) != padding) {
-                    perror("Failed to write to tar archive");
-                    close_file(tar_archive, "Failed to close tar archive");
-                    close_file(input_file, "Failed to close file");
-                    return -1;
-                }
-            }
-        }
-
-        // Check If The fread Function Failed To Read The File.
-        // If The Bytes Fetched Is 0 And ferror Returns An Error, Return An Error.
-        if (bytes_fetched == 0 && ferror(input_file) != 0) {
-            perror("Failed to read file");
+        if (write_file_contents(tar_archive, input_file) != 0) {
             close_file(tar_archive, "Failed to close tar archive");
             close_file(input_file, "Failed to close file");
             return -1;
@@ -232,17 +280,9 @@ int create_archive(const char *archive_name, const file_list_t *files) {
         curr_file = curr_file->next;
     }
 
-    // Add Two Empty Blocks (Footer) To The End Of The Archive.
-    char empty_footer[BLOCK_SIZE * NUM_TRAILING_BLOCKS] = {0};
-
-    // Write The Empty Footer To The Archive.
-    // If The Empty Footer Is Not Written To Exactly 1024 Bytes, Return An Error.
-    if (fwrite(empty_footer, 1, BLOCK_SIZE * NUM_TRAILING_BLOCKS, tar_archive) !=
-        BLOCK_SIZE * NUM_TRAILING_BLOCKS) {
-        perror("Failed to write to tar archive");
-        if (fclose(tar_archive) != 0) {
-            perror("Failed to close tar archive");
-        }
+    // Write The Footer (Using The Padding Helper)
+    if (write_footer(tar_archive) != 0) {
+        close_file(tar_archive, "Failed to close tar archive");
         return -1;
     }
 
@@ -313,45 +353,7 @@ int append_files_to_archive(const char *archive_name, const file_list_t *files) 
         }
 
         // Write/Copy The File Contents To The Archive.
-        char buffer[BLOCK_SIZE] = {0};
-        char padding_buffer[BLOCK_SIZE] = {0};
-        size_t bytes_fetched;
-
-        // Read The File Contents In Blocks Of 512 Bytes.
-        // If Bytes Fetched is 0, We Have Reached The End Of The File.
-        while ((bytes_fetched = fread(buffer, 1, BLOCK_SIZE, input_file)) > 0) {
-            // Write The File Contents To The Archive.
-            size_t bytes_written = fwrite(buffer, 1, bytes_fetched, tar_archive);
-
-            // If The Bytes Written Is Not Equal To The Bytes Fetched, Return An Error.
-            if (bytes_written != bytes_fetched) {
-                perror("Failed to write to tar archive");
-                close_file(tar_archive, "Failed to close tar archive");
-                close_file(input_file, "Failed to close file");
-                return -1;
-            }
-
-            // If The File Contents is Less Than 512 Bytes, We need to add padding.
-            // This ensures that the file contents are written in blocks of 512 bytes (following the
-            // tar format).
-            if (bytes_fetched < BLOCK_SIZE) {
-                size_t padding = BLOCK_SIZE - bytes_fetched;
-
-                // Write The Padding To The Archive.
-                // Else If The Padding Is Not Written, Return An Error.
-                if (fwrite(padding_buffer, 1, padding, tar_archive) != padding) {
-                    perror("Failed to write to tar archive");
-                    close_file(tar_archive, "Failed to close tar archive");
-                    close_file(input_file, "Failed to close file");
-                    return -1;
-                }
-            }
-        }
-
-        // Check If The fread Function Failed To Read The File.
-        // If The Bytes Fetched Is 0 And ferror Returns An Error, Return An Error.
-        if (bytes_fetched == 0 && ferror(input_file) != 0) {
-            perror("Failed to read file");
+        if (write_file_contents(tar_archive, input_file) != 0) {
             close_file(tar_archive, "Failed to close tar archive");
             close_file(input_file, "Failed to close file");
             return -1;
@@ -370,14 +372,8 @@ int append_files_to_archive(const char *archive_name, const file_list_t *files) 
         curr_file = curr_file->next;
     }
 
-    // Add Two Empty Blocks (Footer) To The End Of The Archive.
-    char empty_footer[BLOCK_SIZE * NUM_TRAILING_BLOCKS] = {0};
-
-    // Write The Empty Footer To The Archive.
-    // If The Empty Footer Is Not Written To Exactly 1024 Bytes, Return An Error.
-    if (fwrite(empty_footer, 1, BLOCK_SIZE * NUM_TRAILING_BLOCKS, tar_archive) !=
-        BLOCK_SIZE * NUM_TRAILING_BLOCKS) {
-        perror("Failed to write to tar archive");
+    // Write The Footer
+    if (write_footer(tar_archive) != 0) {
         close_file(tar_archive, "Failed to close tar archive");
         return -1;
     }
@@ -416,12 +412,10 @@ int get_archive_file_list(const char *archive_name, file_list_t *files) {
             return -1;
         }
 
-        // We Use strtoull to convert the Octal String to an Unsigned
-        // long long integer. This represents the size of the file.
-        char *end_pointer;
-        size_t file_size = strtoull(archive_header.size, &end_pointer, OCTAL_BASE);
-        if (*end_pointer != '\0') {
-            perror("Failed to convert file size to unsigned long long");
+        // Convert The Octal String To A Size_t.
+        // This is used to calculate the size of the file.
+        size_t file_size;
+        if (convert_octal_to_size_t(archive_header.size, &file_size) != 0) {
             close_file(tar_archive, "Failed to close tar archive");
             return -1;
         }
@@ -454,6 +448,7 @@ int get_archive_file_list(const char *archive_name, file_list_t *files) {
 }
 
 int extract_files_from_archive(const char *archive_name) {
+    // Opening The Existing Tar Archive With Read Permissions.
     FILE *tar_archive = fopen(archive_name, "rb");
     if (tar_archive == NULL) {
         perror("Failed to open tar archive");
@@ -469,53 +464,79 @@ int extract_files_from_archive(const char *archive_name) {
             break;
         }
 
-        // We Use strtoull to convert the Octal String to an Unsigned
-        // long long integer. This represents the size of the file.
-        char *end_pointer;
-        size_t file_size = strtoull(archive_header.size, &end_pointer, OCTAL_BASE);
-        if (*end_pointer != '\0') {
-            perror("Failed to convert file size to unsigned long long");
+        // Convert The Octal String To A Size_t.
+        // This is used to calculate the size of the file.
+        size_t file_size;
+        if (convert_octal_to_size_t(archive_header.size, &file_size) != 0) {
             close_file(tar_archive, "Failed to close tar archive");
             return -1;
         }
 
-        // Open The File That We Want To Write To.
-        // Either will create a new file, or overwrite the existing one.
-        FILE *extract_file = fopen(archive_header.name, "wb");
-        if (extract_file == NULL) {
+        // Open The File To Be Extracted From The Archive.
+        FILE *output_file = fopen(archive_header.name, "wb");
+        if (output_file == NULL) {
             perror("Failed to open file");
             close_file(tar_archive, "Failed to close tar archive");
             return -1;
         }
 
+        // Read The File Contents From The Archive.
         char buffer[BLOCK_SIZE] = {0};
+        size_t bytes_remaining = file_size;
 
-        while ((bytes_fetched = fread(buffer, 1, sizeof(buffer), tar_archive)) > 0) {
-            // Write The File Contents To The Extracted File.
-            size_t bytes_written = fwrite(buffer, 1, bytes_fetched, extract_file);
+        // Read The File Contents In Blocks Of 512 Bytes.
+        // If Bytes Fetched is 0, We Have Reached The End Of The File.
+        while (bytes_remaining > 0) {
+            size_t bytes_to_fetch;
+            if (bytes_remaining < BLOCK_SIZE) {
+                bytes_to_fetch = bytes_remaining;
+            } else {
+                bytes_to_fetch = BLOCK_SIZE;
+            }
 
-            // If The Bytes Written Is Not Equal To The Bytes Fetched, Return An Error.
+            // Read The File Contents From The Archive/Buffer.
+            size_t bytes_fetched = fread(buffer, 1, bytes_to_fetch, tar_archive);
+            if (bytes_fetched != bytes_to_fetch) {
+                perror("Failed to read from tar archive");
+                close_file(tar_archive, "Failed to close tar archive");
+                close_file(output_file, "Failed to close file");
+                return -1;
+            }
+
+            // Write The File Contents To The Output File.
+            size_t bytes_written = fwrite(buffer, 1, bytes_fetched, output_file);
             if (bytes_written != bytes_fetched) {
                 perror("Failed to write to file");
                 close_file(tar_archive, "Failed to close tar archive");
-                close_file(extract_file, "Failed to close file");
+                close_file(output_file, "Failed to close file");
+                return -1;
+            }
+
+            // Update The Bytes Remaining.
+            bytes_remaining -= bytes_fetched;
+        }
+
+        // Handle Padding If The File Size is Not a Multiple of 512,
+        // we need to add padding to reach the next block.
+        if (file_size % BLOCK_SIZE != 0) {
+            size_t padding = BLOCK_SIZE - (file_size % BLOCK_SIZE);
+            if (fseek(tar_archive, padding, SEEK_CUR) != 0) {
+                perror("Failed to seek in tar archive");
+                close_file(tar_archive, "Failed to close tar archive");
+                close_file(output_file, "Failed to close file");
                 return -1;
             }
         }
 
-        size_t spacing = BLOCK_SIZE - (file_size % BLOCK_SIZE);
-        if (spacing != 0) {
-            fseek(tar_archive, spacing, SEEK_CUR);
-        }
-        fseek(tar_archive, spacing, SEEK_CUR);
-
-        if (fclose(extract_file) != 0) {
+        // Close The Output File.
+        if (fclose(output_file) != 0) {
             perror("Failed to close file");
             close_file(tar_archive, "Failed to close tar archive");
             return -1;
         }
     }
 
+    // Close The Tar Archive.
     if (fclose(tar_archive) != 0) {
         perror("Failed to close tar archive");
         return -1;
